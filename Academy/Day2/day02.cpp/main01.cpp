5fc4c38fcb5bb8e10fdf8431c80aabec06c20680
pchanbro <cksgud0509@naver.com>
2024-08-12 19:50:02 +0900

    [지민] 수업내용 커밋

---
+++ Academy/Day2/day02.cpp/main01.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	printf("hello world\n");
+
+	//visual studio 단축기
+	//ctrl + space :인텔리젼스 불러오기(자동으로 입력됨)
+	//드래그상태 + (ctrl + k) + c : 한번에 주석
+	//드래그상태 + (ctrl + k) + u : 주석 풀기
+	//ctrl + shift + f : 원하는 단어 찾기
+	//ctrl + , : 코드 검색
+
+	//ctrl + f5 : 디버깅없이 시작
+	//f5 : 디버깅으로 시작
+
+	// => [ ( ] ) 이거는 안됨 -> [ ( ) ] 이게 맞음
+
+
+
+
+
+
+
+}
+//->주석
+//input->입력
+//output->출력
+
